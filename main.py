84053d08e1b44150d1487ac5476764b5a47bd1dc
wams77 <77565674+wams77@users.noreply.github.com>
2026-08-03 14:08:23 +0800

    Decrease font size from 55 to 50

--- main.py
+++ main.py
@@ -135,7 +135,7 @@ def edit_video_with_captions(video_file, audio_file, script_text, output_file="f
             
             txt_clip = TextClip(
                 text, 
-                fontsize=55, 
+                fontsize=50, 
                 color='white', 
                 font=font_path, 
                 stroke_color='black', 
